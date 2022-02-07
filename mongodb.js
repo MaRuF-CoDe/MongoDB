9ca79658f8a61ca1f3c811353c476ded06a5fa8a
Maruf <alenmaruf.am@gmail.com>
2022-02-08 05:15:38 +0600

    Read - Querying Documents

--- mongodb.js
+++ mongodb.js
@@ -5,15 +5,18 @@ const { MongoClient,ObjectID } = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id.id.length)
-console.log(id.toHexString().length)
+
+// const id = new ObjectID()
+// console.log(id.id.length)
+// console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database!')
     }
     const db = client.db(databaseName)
+
+    //create
 
     // db.collection('user').insertOne({
     //     name : 'Maruf',
@@ -58,4 +61,29 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     //     console.log(result.insertedCount)
     // })
 
+
+
+//Read
+
+
+
+// db.collection('user').findOne({ _id: new ObjectID("62018e5ebc942c70534a2720") }, (error, user) => {
+//         if (error) {
+//             return console.log('Unable to fetch')
+//         }
+
+//         console.log(user)
+//     })
+
+//     db.collection('user').find({ age: 25 }).toArray((error, users) => {
+//         console.log(users)
+//     })
+
+    db.collection('tasks').findOne({ _id: new ObjectID("6201933b7c3452e7cab21778") }, (error, task) => {
+        console.log(task)
+    })
+
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        console.log(tasks)
+    })
 })
