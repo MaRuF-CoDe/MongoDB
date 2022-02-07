db8de5a5af58510e46eb6a5837ce055a72ea8522
Maruf <alenmaruf.am@gmail.com>
2022-02-08 03:47:48 +0600

    Inserting more documents to database

--- mongodb.js
+++ mongodb.js
@@ -6,14 +6,53 @@ const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL,{ useNewUrlParser: true},(error,client)=>{
-    if(error){
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
         return console.log('Unable to connect to database!')
     }
     const db = client.db(databaseName)
 
-    db.collection('user').insertOne({
-        name : 'Maruf',
-        age : 25
+    // db.collection('user').insertOne({
+    //     name : 'Maruf',
+    //     age : 25
+    // })
+    // db.collection('user').insertMany([
+    //     {
+    //         name: 'Tahmid',
+    //         age: 35,
+    //         occupation: 'Student'
+    //     },
+    //     {
+    //         name: 'Bijon',
+    //         age: 23,
+    //         Hobby: 'Doing Everything'
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result.insertedCount)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            descriptio: 'Cleaning',
+            completed: true
+        },
+        {
+            descriptio: 'Inspection',
+            completed: false
+        }, {
+            descriptio: 'Planting',
+            completed: false
+        },
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert documents!')
+        }
+
+        console.log(result.insertedCount)
     })
+
 })
